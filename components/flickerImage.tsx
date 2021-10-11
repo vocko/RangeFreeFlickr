ca846195dcd661e8c3ae99a13db38d828cd48379
Vladimir Palocko <vlad@nimbusportals.com>
2021-10-11 17:45:08 +1000

    Display all tags on hover

--- components/flickerImage.tsx
+++ components/flickerImage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { FlickrImageData } from '../types';
 
 import styles from '../styles/FlickrImage.module.css'
@@ -11,6 +11,7 @@ const FlickrImage: React.FC<FlickerImageProps> = (props: FlickerImageProps) => {
 
     const date = new Date(props.data.date_taken);
     const [tags, setTags] = useState<string>(props.data.tags);
+    const allTagesRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
 
@@ -18,6 +19,18 @@ const FlickrImage: React.FC<FlickerImageProps> = (props: FlickerImageProps) => {
             setTags(props.data.tags.substring(0, 79) + '...');
 
     }, [props.data.tags])
+
+    const handleMouseOverTags = (e: React.MouseEvent) => {
+
+        if (allTagesRef.current && props.data.tags.length > 80)
+            allTagesRef.current.classList.add(styles.show);
+    };
+
+    const handleMouseOutTags = (e: React.MouseEvent) => {
+        
+        if (allTagesRef.current)
+            allTagesRef.current.classList.remove(styles.show);
+    }
 
     return <div className={styles.imageContainer} key={props.data.link}>
         <a href={props.data.link} target="_blank" rel="noreferrer">
@@ -27,10 +40,11 @@ const FlickrImage: React.FC<FlickerImageProps> = (props: FlickerImageProps) => {
         <div className={styles.taken}>on {date.toLocaleDateString()} at {date.toLocaleTimeString()}</div>
         {
             tags.length > 0 
-                ? <div className={styles.tags}><strong>Tags: </strong>{tags}</div> 
+                ? <div className={styles.tags} onMouseOver={handleMouseOverTags}><strong>Tags: </strong>{tags}</div> 
                 : null
         }
         <div className={styles.spacer}></div>
+        <div className={styles.allTags} ref={ref => allTagesRef.current = ref} onMouseOut={handleMouseOutTags}><strong>Tags: </strong>{props.data.tags}</div>
     </div>;
 };
 
